a9a9c127da043ed6d4e8a6e8f22deb0e6f8232a7
Uttam <uttamrohit4545@gmail.com>
2025-08-12 23:00:05 +0600

    feat : fully mongodb setup and mongodb compas

--- data-modeling-schema/index.js
+++ data-modeling-schema/index.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
+require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = 3000
+
+
+//middleware
+app.use(express.json());
+app.use(cors())
 
 //connecting to mongodb
 const uri = process.env.MONGODB_URL;
@@ -19,12 +26,33 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    const db = client.db("data-modeling");
+    const usersCollection = db.collection("users");
+
+
+    //create a user
+    app.post("/create-user", async(req,res) => {
+
+        try{
+            const user = await usersCollection.insertOne(req.body);
+            res.send(user);
+
+        }catch(error){
+            res.status(404).json({
+                message : "not successfull"
+            })
+        }
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
